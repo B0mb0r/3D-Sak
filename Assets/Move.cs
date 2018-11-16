26d25f0f2b7978be9ebcf03953ac6f3a7b1d55e5
hugo fagerlind <hugo.fagerlind@elev.ga.lbs.se>
2018-11-16 15:53:07 +0100

    3D-TiltPewPewGame

---
+++ Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    CharacterController cC;
+    public float Speed;
+    public float turnSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        cC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertInput = Input.GetAxis("Vertical");
+        float horizInput = Input.GetAxis("Horizontal");
+        cC.SimpleMove(transform.forward * vertInput * Speed);
+        transform.Rotate(Vector3.up * horizInput * turnSpeed);
+    }
+}
